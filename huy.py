d7c141f130819ff85008adafe418df458363dbbe
Huy Truong <truongkhaihuy1211@gmail.com>
2023-10-09 20:12:27 +0300

    Completed checkname def

--- huy.py
+++ huy.py
@@ -1,3 +1,45 @@
+from config import connection
+# print("Welcome to the Game or sth!")
+# print("Press 1. for New Game")
+# print("Press 2. to Continue")
+# print("Press 3. to Exit")
+# option = int(input("Enter option: "))
+def start():
+
+
+    if option == 1:
+        print("Create username for a new game:")
+        username = input("Enter username: ")
+        print(f"Welcome, {username}! Let's start!")
+        sql = f"INSERT INTO player(name) VALUES ('{username}')"
+
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        connection.commit()  # Commit the transaction
+
+    elif option == 2:
+        existing_user = input("Enter existing username: ")
+        sql = f"SELECT * FROM player WHERE name = ('{existing_user}')"
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        if result:
+            print(f"Welcome back {existing_user}!")
+
+
+    elif option == 3:
+        print("Exiting Game!")
+# start()
+def player():
+    sql= "select player.name from player;"
+    cursor= connection.cursor()
+    cursor.execute(sql)
+    result= cursor.fetchall()
+    if cursor.rowcount > 0:
+        for x in result:
+            print(f"{x[0]}")
+    return
+# player()
 def nameofuser():
     listuser = []
     while True:
@@ -33,4 +75,58 @@ def nameofuser():
             print("Start new game !")
             listuser.append(username)
             break
-nameofuser()
+# nameofuser()
+
+
+
+def getPlayers():
+    sql = "SELECT * from player"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    # print("this is",result)
+    # result =runSQL(sql)
+    return result
+def isExist(username,namesInDatabase ):
+    if username in namesInDatabase:
+        return True
+    else:
+        return False
+def inputName():
+     name= input("Type your username: ")
+     return name
+def formatedNameList(nameListFromDatabase):
+    onlyNameList = []
+    for nameData in nameListFromDatabase:
+        nameInData = nameData[1]
+        # print(nameInData)
+        onlyNameList.append(nameInData)
+    # print(onlyNameList)
+    return onlyNameList
+def game():
+    nameList = getPlayers() ## lay nameList
+    onlyNameList = formatedNameList(nameList)
+    print(onlyNameList)
+    name = inputName() ## user dien ten
+
+    # formatedList = formatedNameList(nameList) # format nameList
+    # print(formatedList)
+    # return nameinData
+
+    nameExist = isExist(name,onlyNameList) #return true or false
+    print(nameExist)
+    # while nameExist: # if name is existed
+    #     print("Name already used")
+    #     print("Type another name")
+game()
+
+    # nameExist = isExist(result1,name) # true / false
+    # if nameExist == True:
+    #     print("* Name already used")
+    #     print("* Please type again")
+    #     return
+    # else:
+    #     return
+# print(getPlayers())
+
+
